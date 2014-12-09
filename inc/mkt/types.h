bfd3af4964f8279408a2f853b616edc3e7b39fcf
Joe R <transfix@sublevels.net>
2014-12-09 10:40:20 -0600

    Moving types to their own header. Will split up app.h and app.cpp accordingly.

---
+++ inc/mkt/types.h
@@ -0,0 +1,22 @@
+#ifndef __MKT_TYPES_H__
+#define __MKT_TYPES_H__
+
+#include <mkt/config.h>
+
+#include <boost/cstdint.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/shared_mutex.hpp>
+
+//basic types used by the system
+namespace mkt
+{
+  typedef boost::int64_t             int64;
+  typedef boost::uint64_t            uint64;
+  typedef boost::posix_time::ptime   ptime;
+  typedef boost::shared_mutex        mutex;
+  typedef boost::unique_lock<mutex>  unique_lock;
+  typedef boost::shared_lock<mutex>  shared_lock;
+}
+
+#endif
